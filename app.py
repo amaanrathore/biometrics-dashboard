b6600b44102de12f75427450065a2bdeaf80a9b5
amaanrathore <158060745+amaanrathore@users.noreply.github.com>
2025-07-23 16:18:41 +0530

    app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ CORS(app, resources={
         "origins": [
             "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "https://biometrics-dashboard-iq8y63fym-amaan-rathores-projects.vercel.app", 
+            "https://biometrics-dashboard-dxqtiukks-amaan-rathores-projects.vercel.app", 
             "https://biometrics-dashboard.vercel.app", # Exact Vercel URL
             "https://biometrics-dashboard-git-main-amaan-rathores-projects.vercel.app"
         ],
